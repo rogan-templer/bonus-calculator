c96e0bc3f45cb0570ba24634a60d40eb29d12a23
Rogan-Templer <rmtempler@gmail.com>
2021-08-31 12:29:06 +1200

    global state file updated

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { createContext } from "react";
 
 //Initial State
 
-const reward = {
+const reward = [
+  {
   sales: 100,
   freeCalls: 55,
   totalCalls: 1815,
   npsTotal: 100,
   appsConvertedTotal: 100,
-};
+}
+];
+
+// Create context
+export const GlobalContext = createContext(reward);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, reward);
