cdf70205998f7c3b14021c4f517b5589331e12e0
Rogan-Templer <rmtempler@gmail.com>
2021-08-31 15:04:00 +1200

    global state file simplified for now

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,15 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 //Initial State
 
 const initialState = {
   kpis: [
-    { type: "Sales", target: 100 },
-    { type: "freeCalls", target: 55 },
-    { type: "totalCalls", target: 1815 },
-    { type: "npsTotal", target: 100 },
-    { type: "appsConvertedTotal", target: 100 },
+    { id: 1, type: "Sales", target: 100 },
+    { id: 2, type: "freeCalls", target: 55 },
+    { id: 3, type: "totalCalls", target: 1815 },
+    { id: 4, type: "npsTotal", target: 100 },
+    { id: 5, type: "appsConvertedTotal", target: 100 },
   ],
 };
 
@@ -20,46 +20,44 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  // Actions
-  function salesPercentage(sales) {
-    dispatch({
-      type: "SALES_PERCENTAGE",
-      payload: sales,
-    });
-  }
+  // // Actions
+  // function salesPercentage(sales) {
+  //   dispatch({
+  //     type: "SALES_PERCENTAGE",
+  //     payload: sales,
+  //   });
+  // }
 
-  function freeCallsPercentage(freeCalls) {
-    dispatch({
-      type: "FREE_CALLS_PERCENTAGE",
-      payload: freeCalls,
-    });
-  }
+  // function freeCallsPercentage(freeCalls) {
+  //   dispatch({
+  //     type: "FREE_CALLS_PERCENTAGE",
+  //     payload: freeCalls,
+  //   });
+  // }
 
-  function totalCallsPercentage(totalCalls) {
-    dispatch({
-      type: "TOTAL_CALLS_PERCENTAGE",
-      payload: totalCalls,
-    });
-  }
-  function npsPercentage(npsTotal) {
-    dispatch({
-      type: "NPS_PERCENTAGE",
-      payload: npsTotal,
-    });
-  }
-  function appsConvertedPercentage(appsConvertedTotal) {
-    dispatch({
-      type: "APPS_CONVERTED_PERCENTAGE",
-      payload: appsConvertedTotal,
-    });
-  }
+  // function totalCallsPercentage(totalCalls) {
+  //   dispatch({
+  //     type: "TOTAL_CALLS_PERCENTAGE",
+  //     payload: totalCalls,
+  //   });
+  // }
+  // function npsPercentage(npsTotal) {
+  //   dispatch({
+  //     type: "NPS_PERCENTAGE",
+  //     payload: npsTotal,
+  //   });
+  // }
+  // function appsConvertedPercentage(appsConvertedTotal) {
+  //   dispatch({
+  //     type: "APPS_CONVERTED_PERCENTAGE",
+  //     payload: appsConvertedTotal,
+  //   });
+  // }
 
   return (
     <GlobalContext.Provider
       value={{
-        transactions: state.transactions,
-        deleteTransaction,
-        addTransaction,
+        kpis: state.kpis,
       }}
     >
       {children}
