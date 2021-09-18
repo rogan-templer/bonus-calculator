87dc08c35967e976456c0bc2b9b45d9e5f7be14d
Rogan-Templer <rmtempler@gmail.com>
2021-09-18 16:45:13 +1200

    change to layout of results component

--- src/components/PercentResult.jsx
+++ src/components/PercentResult.jsx
@@ -34,77 +34,75 @@ function PercentResult() {
     <>
       <div className="mt-3">
         <h4>Outcomes</h4>
-        <ul>
+        <span>
           {sales.map((sale) => {
             return (
               <div key={sale.id}>
                 <h4>Sales</h4>
-                <li>{sale.text}</li>
-                <li>{sale.target}</li>
-                <li>{sale.actual}</li>
-                <li>{Math.floor(sale.salesPercentage * 100)}%</li>
+                <p>{sale.text}</p>
+                <p>{sale.target}</p>
+                <p>{sale.actual}</p>
+                <p>{Math.floor(sale.salesPercentage * 100)}%</p>
               </div>
             );
           })}
-        </ul>
+        </span>
 
-        <ul>
+        <span>
           {totalCalls.map((totalCall) => {
             return (
               <div key={totalCall.id}>
                 <h4>Total Calls</h4>
-                <li>{totalCall.text}</li>
-                <li>{totalCall.target}</li>
-                <li>{totalCall.actual}</li>
-                <li>{Math.floor(totalCall.totalCallsPercentage * 100)}%</li>
+                <p>{totalCall.text}</p>
+                <p>{totalCall.target}</p>
+                <p>{totalCall.actual}</p>
+                <p>{Math.floor(totalCall.totalCallsPercentage * 100)}%</p>
               </div>
             );
           })}
-        </ul>
+        </span>
 
-        <ul>
+        <span>
           {freeCalls.map((freeCall) => {
             return (
               <div key={freeCall.id}>
                 <h4>Free Calls</h4>
-                <li>{freeCall.text}</li>
-                <li>{freeCall.target}</li>
-                <li>{freeCall.actual}</li>
-                <li>{Math.floor(freeCall.freeCallsPercentage * 100)}%</li>
+                <p>{freeCall.text}</p>
+                <p>{freeCall.target}</p>
+                <p>{freeCall.actual}</p>
+                <p>{Math.floor(freeCall.freeCallsPercentage * 100)}%</p>
               </div>
             );
           })}
-        </ul>
+        </span>
 
-        <ul>
+        <span>
           {appsConverted.map((appsConvert) => {
             return (
               <div key={appsConvert.id}>
                 <h4>Applications Converted</h4>
-                <li>{appsConvert.text}</li>
-                <li>{appsConvert.target}</li>
-                <li>{appsConvert.actual}</li>
-                <li>
-                  {Math.floor(appsConvert.appsConvertedPercentage * 100)}%
-                </li>
+                <p>{appsConvert.text}</p>
+                <p>{appsConvert.target}</p>
+                <p>{appsConvert.actual}</p>
+                <p>{Math.floor(appsConvert.appsConvertedPercentage * 100)}%</p>
               </div>
             );
           })}
-        </ul>
+        </span>
 
-        <ul>
+        <span>
           {nps.map((np) => {
             return (
               <div key={np.id}>
                 <h4>Net Promoter Score</h4>
-                <li>{np.text}</li>
-                <li>{np.target}</li>
-                <li>{np.actual}</li>
-                <li>{Math.floor(np.npsPercentage * 100)}%</li>
+                <p>{np.text}</p>
+                <p>{np.target}</p>
+                <p>{np.actual}</p>
+                <p>{Math.floor(np.npsPercentage * 100)}%</p>
               </div>
             );
           })}
-        </ul>
+        </span>
         <div>
           <h1>Overall Score: {Math.floor(results)} %</h1>
         </div>
